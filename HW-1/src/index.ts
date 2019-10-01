f3c63e533ce4e37621fe5ee510456eb26094c9c9
Hanna Hlibova <ahatminskaya@gmail.com>
2019-10-01 11:07:30 +0300

    first task fixes

--- HW-1/src/index.ts
+++ HW-1/src/index.ts
@@ -1,4 +1,4 @@
-function isPalindrome(str:string) {
+function isPalindrome(str:string):boolean {
 	const strReverse = str.split('').reverse().join('');
 
 	if(strReverse === str) {
@@ -11,21 +11,26 @@ function isPalindrome(str:string) {
 const testString = isPalindrome('abba');
 const testString2 = isPalindrome('azbba');
 
-function getFibArr(num:number) {
-	const fibArr = [];
+function getFibArr(num:number):number[] {
+	const fibArr:number[] = [];
 	let sum = 1;
 
-	if(num>0) {
-		for( let i=0; i<num; i++) {
-			if( fibArr.length > 1) {
-				sum += fibArr[ fibArr.length-2]
-			};
-			if( fibArr.length === 1) {
-				sum = 1
-			};
+	if (num<0) {
+		return fibArr;
+	}
+	
+	for (let i=0; i<num; i++) {
+		const fibArrLength = fibArr.length;
+		
+		if (fibArrLength > 1) {
+			sum += fibArr[fibArrLength - 2];
+		};
+		
+		if (fibArrLength === 1) {
+			sum = 1;
+		};
 
-			fibArr.push( sum);
-		}
+		fibArr.push(sum);
 	}
 
 	return fibArr;
